07c9c1a9019f1c2a58d248605583f11ea8453cce
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-07-28 19:13:37 +0530

    Fix text

--- hirableFinal/components/LandingPage/LandingPage.jsx
+++ hirableFinal/components/LandingPage/LandingPage.jsx
@@ -96,7 +96,7 @@ const LandingPage = () => {
     <div className="bg-white">
       <div className="home-img border-0 bg-no-repeat bg-cover bg-100 h-[100vh] sm-mid-tablet:bg-bottom bg-right">
         <div className="sm:grid sm:grid-cols-10 sm:gap-3 md:gap-6 mx-auto max-w-screen-xl px-6 md:px-12">
-          <section className="col-span-6 lg:col-span-4 pt-[19%]">
+          <section className="col-span-10 lg:col-span-4 pt-[19%]">
             <h1 className="text-5xl tracking-tight leading-leading-first font-bold mb-8 sm-mid-tablet:text-3xl sm-mid-tablet:leading-snug">
               Bridging companies and campuses to
               <span className="text-purple-500"> empower students</span>
