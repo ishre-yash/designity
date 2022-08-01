9c2939f667aa140463179c3e367f9cf1604dd7b0
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-08-01 22:23:27 +0530

    update

--- hirableFinal/components/students/Hero1.jsx
+++ hirableFinal/components/students/Hero1.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 function Hero1() {
   return (
     <>
-      <section className="bg-[#f4efe9]">
+      <section className="bg-[#f4efe9] font-gazpacho ">
         <div className="relative py-8 md:py-12">
           <div className="relative mx-auto max-w-7xl px-4 lg:px-4">
             <div className="grid rounded-2xl pt-14 lg:min-h-[540px] lg:grid-cols-2 lg:grid-rows-1 lg:items-center lg:py-0 lg:text-left bg-white text-center">
               <div className="px-5 sm:px-10 md:px-16 md:py-10 ">
-                <h1 className="font-semibold antialiased text-[40px] tracking-tight leading-[50px] xl:text-[48px] xl:leading-[58px] md:whitespace-pre-wrap text-[#1E1928]">
+                <h1 className="font-semibold antialiased text-[40px] tracking-wider leading-[50px] xl:text-[48px] xl:leading-[58px] md:whitespace-pre-wrap text-[#1E1928] ">
                   Get hired by companies looking for diverse talent
                 </h1>
-                <p className="mb-4 text-lg tracking-tighter whitespace-pre-wrap py-4 md:mb-0 md:py-8 text-[#1E1928]">
-                  We are creatingafuture where students are rewarded in the
+                <p className="  mb-4 text-lg tracking-wider whitespace-pre-wrap py-4 md:mb-0 md:py-8 text-[#57545f]">
+                  We are creating a future where students are rewarded in the
                   pursuit of their career based on their abilities and
                   experience.
                 </p>
@@ -45,14 +45,14 @@ function Hero1() {
                   <div className="md:mb-4 flex items-center justify-start md:justify-center">
                     <img
                       className="w-[60px] h-[60px] "
-                      src="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/410.svg"
+                      src="https://www.empathy.com/images/home/holistic_care.svg"
                       alt=""
                     />
                   </div>
                   <div className="text-[24px] leading-[36px] font-semibold tracking-tight md:text-[32px] md:leading-[42px] mb-2 md:mb-4">
                     Startup Connect
                   </div>
-                  <div className="text-lg antialiased tracking-tighter text-[#1e1928cc] ">
+                  <div className="text-lg antialiased tracking-wider text-[#1e1928cc] ">
                     Connect with local innovators and entrepreneurs to discover
                     the startups creatingadifference
                   </div>
@@ -63,14 +63,14 @@ function Hero1() {
                   <div className="md:mb-4 flex items-center justify-start md:justify-center">
                     <img
                       className="w-[60px] h-[60px] "
-                      src="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/410.svg"
+                      src="https://www.empathy.com/images/home/less_stress.svg"
                       alt=""
                     />
                   </div>
                   <div className="text-[24px] leading-[36px] font-semibold tracking-tight md:text-[32px] md:leading-[42px] mb-2 md:mb-4">
                     Recruiter Connect
                   </div>
-                  <div className="text-lg antialiased tracking-tighter text-[#1e1928cc] ">
+                  <div className="text-lg antialiased tracking-wider text-[#1e1928cc] ">
                     Connect with hiring managers and recruiters to find where
                     you belong
                   </div>
@@ -81,17 +81,92 @@ function Hero1() {
                   <div className="md:mb-4 flex items-center justify-start md:justify-center">
                     <img
                       className="w-[60px] h-[60px] "
-                      src="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/410.svg"
+                      src="https://www.empathy.com/images/home/better_outcome.svg"
                       alt=""
                     />
                   </div>
                   <div className="text-[24px] leading-[36px] font-semibold tracking-tight md:text-[32px] md:leading-[42px] mb-2 md:mb-4">
                     TalentCon
                   </div>
-                  <div className="text-lg antialiased tracking-tighter text-[#1e1928cc] ">
+                  <div className="text-lg antialiased tracking-wider text-[#1e1928cc] ">
                     First of its kind hiring event that introduces the best
                     talents to companies by taking hiring to the next level
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="relative py-8 md:py-12">
+          <div className="relative mx-auto max-w-7xl px-4 lg:px-4">
+            <h2 className="font-semibold antialiased text-[40px] tracking-tight leading-[50px] xl:text-[48px] xl:leading-[58px] md:whitespace-pre-wrap text-[#1E1928] text-center my-8">
+              Core values
+            </h2>
+            <div className="mx-auto grid gap-y-14 gap-x-32 sm:grid-cols-2 sm:gap-y-24 md:max-w-5xl">
+              <div className="flex max-w-lg flex-col items-start md:flex-row">
+                <div className="mr-8 w-10 md:w-auto">
+                  <img src="https://www.empathy.com/images/tree1.svg" alt="" />
+                </div>
+                <div className="flex flex-1 flex-col self-start">
+                  <h3 className="text-[24px] leading-[34px] font-semibold text-[#1E1928] mb-2">
+                    Leadership as Experience
+                  </h3>
+                  <p className="text-[16px] leading-[24px] md:leading-[30px] md:text-[18px] tracking-wide antialiased text-[#1e1928cc]">
+                    Students&apos;volunteering activities have been of interest
+                    to academics,but the impact of these activities on learning
+                    and employability has been neglected. Through our platform
+                    students can present that category of experience ina number
+                    of ways and get rewarded with opportunities form companies.
+                  </p>
+                </div>
+              </div>
+              <div className="flex max-w-lg flex-col items-start md:flex-row">
+                <div className="mr-8 w-10 md:w-auto">
+                  <img src="https://www.empathy.com/images/tree2.svg" alt="" />
+                </div>
+                <div className="flex flex-1 flex-col self-start">
+                  <h3 className="text-[24px] leading-[34px] font-semibold text-[#1E1928] mb-2">
+                    Companies apply to you
+                  </h3>
+                  <p className="text-[16px] leading-[24px] md:leading-[30px] md:text-[18px] tracking-wide antialiased text-[#1e1928cc]">
+                    Gone are days when students apply to tens of companies and
+                    sit for rounds of interviews giving assessments.On
+                    Hirable,you don&apos;t need to apply to companies because
+                    companies apply to you
+                  </p>
+                </div>
+              </div>
+              <div className="flex max-w-lg flex-col items-start md:flex-row">
+                <div className="mr-8 w-10 md:w-auto">
+                  <img src="https://www.empathy.com/images/tree3.svg" alt="" />
+                </div>
+                <div className="flex flex-1 flex-col self-start">
+                  <h3 className="text-[24px] leading-[34px] font-semibold text-[#1E1928] mb-2">
+                    Experience based hiring
+                  </h3>
+                  <p className="text-[16px] leading-[24px] md:leading-[30px] md:text-[18px] tracking-wide antialiased text-[#1e1928cc]">
+                    On Hirable,you don&apos;t need to give rounds of interviews
+                    and assessments to get selected for an internship orafull
+                    time role.Instead,we simulate your experience for the role
+                    you are interested to apply and depending on your
+                    performance in the real time environment you will be
+                    directly sent to the final round
+                  </p>
+                </div>
+              </div>
+              <div className="flex max-w-lg flex-col items-start md:flex-row">
+                <div className="mr-8 w-10 md:w-auto">
+                  <img src="https://www.empathy.com/images/tree4.svg" alt="" />
+                </div>
+                <div className="flex flex-1 flex-col self-start">
+                  <h3 className="text-[24px] leading-[34px] font-semibold text-[#1E1928] mb-2">
+                    Right to opportunity
+                  </h3>
+                  <p className="text-[16px] leading-[24px] md:leading-[30px] md:text-[18px] tracking-wide antialiased text-[#1e1928cc]">
+                    Education should not only give you knowledge but also an
+                    experience and an opportunity to work with companies while
+                    you are in college itself.
+                  </p>
                 </div>
               </div>
             </div>
