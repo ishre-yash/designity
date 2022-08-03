951649c2bc64ef79d46bfd9e0aeaf87dd35ca570
Shre Yash <88998531+ishre-yash@users.noreply.github.com>
2022-08-03 09:04:05 +0530

    project change

--- MetaDev/src/components/Profile/ProfileBio.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ProfileBio = () => {
-  return <div>ProfileBio</div>;
-};
-
-export default ProfileBio;
--- hirableFinal/pages/_app.jsx
+++
@@ -1,6 +0,0 @@
-import '../styles/globals.css';
-import '../styles/animations.css';
-
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
-
-export default App;
